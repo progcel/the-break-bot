927095f7e7ad8a66e1a5efccc2e5fa7660d4357b
progcel <61097796+progcel@users.noreply.github.com>
2020-04-24 12:44:44 +0300

    Retry API calls

--- apicaller.js
+++ apicaller.js
@@ -1,17 +1,20 @@
 const axios = require('axios');
+const axiosRetry = require('axios-retry');
 
-const break_channel_id = 'UC6Ff1z4MhKDDLEUqH_hisyQ';
-const youtube_api = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${break_channel_id}&maxResults=1&order=date&key=${process.env.YOUTUBE_API_TOKEN}`
-const youtube_stats_api = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${break_channel_id}&key=${process.env.YOUTUBE_API_TOKEN}`
-const youtube_prefix = 'https://www.youtube.com/watch?v='
+const breakChannelId = 'UC6Ff1z4MhKDDLEUqH_hisyQ';
+const youtubeApi = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${breakChannelId}&maxResults=1&order=date&key=${process.env.YOUTUBE_API_TOKEN}`
+const youtubeStatsApi = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${breakChannelId}&key=${process.env.YOUTUBE_API_TOKEN}`
+const youtubePrefix = 'https://www.youtube.com/watch?v='
+
+axiosRetry(axios, { retries: 5 });
 
 module.exports = {
   getVideoInfo: async function () {
     try {
-      let response = await axios.get(youtube_api);
+      let response = await axios.get(youtubeApi);
       
       return {
-        url: `${youtube_prefix}${response.data.items[0].id.videoId}`,
+        url: `${youtubePrefix}${response.data.items[0].id.videoId}`,
         publishedAt: response.data.items[0].snippet.publishedAt,
       };
     } catch (e) {
@@ -21,7 +24,7 @@ module.exports = {
 
   getStats: async function () {
     try {
-      let response = await axios.get(youtube_stats_api);
+      let response = await axios.get(youtubeStatsApi);
       
       return {
         viewCount: response.data.items[0].statistics.viewCount,
